356ff8901636f6b597dc28e4c68cb70490ef01a7
Kamigad <camilo971939@gmail.com>
2026-09-12 16:38:51 -0500

    creacion de la capa controlador

---
+++ recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,25 @@
+package gm.rh.controlador;
+
+import gm.rh.modelo.Empleado;
+import gm.rh.servicio.IEmpleadoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/empleados") // http:localhost:8080/api/empleados
+@CrossOrigin(value = "http://localhost:3000")
+public class EmpleadoControlador {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
+    private final IEmpleadoServicio iEmpleadoServicio;
+
+    public EmpleadoControlador(IEmpleadoServicio iEmpleadoServicio){
+        this.iEmpleadoServicio = iEmpleadoServicio;
+    }
+}
